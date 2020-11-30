f3831cf084a1e4791da017dc933f69ef5cc24b02
jGiova <josue@sisbps.com.ar>
2020-11-30 20:24:36 -0300

    limpio codigo

--- Regresion/string_hash_more.c
+++ Regresion/string_hash_more.c
@@ -10,22 +10,6 @@ string_hash_init(string_hash *h)
 	h->hash = 0;
 	h->size = 0;
 }
-/*
-void
-string_hash_more(string_hash *sh, char *str, size_t len)
-{
-	assert(sh->flag == STRING_HASH_INIT || sh->flag == STRING_HASH_MORE);
-
-	if (sh->flag == STRING_HASH_INIT) {
-		sh->flag = STRING_HASH_MORE;
-		sh->hash = (*str) << 7;
-	}
-
-	while ((*str) != 0 && len--) {
-		sh->hash = (1000003 * sh->hash) ^ *str++;
-		sh->size++;
-	}
-}*/
 
 void
 string_hash_done(string_hash *sh)
